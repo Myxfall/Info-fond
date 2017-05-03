bc030d3d50d8bea0e8c0167e180b0d76730dac9b
Myxfall <maximilienromain96@hotmail.com>
2017-05-03 13:32:34 +0200

    adding model tour

--- question3Choco/Echec.java
+++ question3Choco/Echec.java
@@ -1,5 +1,9 @@
 package question3Choco;
 
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.variables.IntVar;
+
 public class Echec {
 	String mode;
 	
@@ -11,4 +15,22 @@ public class Echec {
 		
 	}
 
+	public void impedence(){
+		int n = 4;
+		Model model = new Model(n + "-independance problem");
+		IntVar[] vars = new IntVar[n];
+		for(int q = 0; q < n; q++){
+		    vars[q] = model.intVar("T_"+q, 1, n); //contrainte tour, une tour par ligne
+		}
+		for(int i  = 0; i < n-1; i++){
+		    for(int j = i + 1; j < n; j++){
+		    	model.arithm(vars[i], "!=", vars[j]).post();
+		    }
+		}
+		Solution solution = model.getSolver().findSolution();
+		if(solution != null){
+		    System.out.println(solution.toString());
+		}
+	}
+	
 }
