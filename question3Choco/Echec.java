b2251415f39427d44a066770be4d3b82b51c6f24
George Rusu <georgesrusu@yahoo.fr>
2017-05-03 18:08:29 +0200

    tour fonctionne

--- question3Choco/Echec.java
+++ question3Choco/Echec.java
@@ -2,35 +2,87 @@ package question3Choco;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.variables.IntVar;
 
 public class Echec {
-	String mode;
+	private int n;
+	private int t;
+	private int f;
+	private int c;
 	
 	
-	public Echec(String[] args){
-		this.mode=args[0];
-		System.out.print(this.mode);
+	
+	public Echec(int n,int t, int f, int c){
+		this.n=n;
+		this.t=t;
+		this.f=f;
+		this.c=c;
 		
 		
 	}
+	private int calcT(IntVar[][] echequier){
+		int count=0;
+		for (int i=0;i<this.n;i++){
+			for (int j=0;j<this.n;j++){
+				if (echequier[i][j].contains(1)){
+					count+=1;
+				}
+			}
+		}
+		return count;
+	}
+	
+	private void printingBoard(IntVar[][] echequier){
+		String line;
+		for (int i=0;i<this.n;i++){
+			line="";
+			for (int j=0;j<this.n;j++){
+				if (echequier[i][j].contains(0)){
+					line+="*";
+				}else if (echequier[i][j].contains(1)){
+					line+="T";
+				}else if (echequier[i][j].contains(2)){
+					line+="F";
+				}else if (echequier[i][j].contains(2)){
+					line+="C";
+				}
+				line+="  ";
+			}
+			System.out.println(line);
+		}
+	}
 
-	public void impedence(){
-		int n = 4;
-		Model model = new Model(n + "-independance problem");
-		IntVar[] vars = new IntVar[n];
-		for(int q = 0; q < n; q++){
-		    vars[q] = model.intVar("T_"+q, 1, n); //contrainte tour, une tour par ligne
+	public void independance(){
+		Model model = new Model(this.n + "-independance problem");
+		IntVar[][] echequier =model.intVarMatrix("echequier",this.n,this.n,0,3); //0=Vide 1=tour 2=fou 3=cavalier
+		for (int i=0;i<this.n;i++){
+			for (int j=0;j<this.n;j++){
+				for (int l=i+1; l<this.n;l++){
+					//contraintes pour la tour
+					model.ifThen(model.arithm(echequier[i][j],"=", 1), model.arithm(echequier[i][l], "=", 0));
+					model.ifThen(model.arithm(echequier[i][j],"=", 1), model.arithm(echequier[l][i], "=", 0));
+					model.arithm(model.intVar(this.calcT(echequier)), "<=", this.t);
+					
+					//contraintes pour le cavalier
+					
+				}
+			}
+			
 		}
-		for(int i  = 0; i < n-1; i++){
-		    for(int j = i + 1; j < n; j++){
-		    	model.arithm(vars[i], "!=", vars[j]).post();
-		    }
+		
+		
+		//Solution solution = model.getSolver().findSolution();
+		Solver solver = model.getSolver();
+		if(solver.solve()){
+		    this.printingBoard(echequier);
+		}else {
+		    System.out.println("The solver has proved the problem has no solution");
 		}
-		Solution solution = model.getSolver().findSolution();
-		if(solution != null){
-		    System.out.println(solution.toString());
-		}
+		
+	}
+	public void domination(){
+		
 	}
 	
 }
--- question3Choco/Main.java
+++ question3Choco/Main.java
@@ -1,13 +1,40 @@
 package question3Choco;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		//Echec jeu =new Echec(args);
-		JCommander.newBuilder().addObject(args).build().parse(args);
+	public static void main(String[] args) throws ParseException {
+		Options options = new Options();
+		options.addOption("i", false, "mode independance");
+		options.addOption("d", false, "mode domination");
+		options.addOption("n", true, "longueure de l'echequier");
+		options.addOption("t", true, "nombre de tour");
+		options.addOption("f", true, "nombre de fou");
+		options.addOption("c", true, "nombre de chevalier");
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse( options, args);
+		
+		int n=Integer.parseInt(cmd.getOptionValue("n"));
+		int t=Integer.parseInt(cmd.getOptionValue("t"));
+		int f=Integer.parseInt(cmd.getOptionValue("f"));
+		int c=Integer.parseInt(cmd.getOptionValue("c"));
+		
+		Echec jeu =new Echec(n,t,f,c);
+		
+		if(cmd.hasOption("-i")) {
+			jeu.independance();
+		}
+		else if (cmd.hasOption("-d")){
+			jeu.domination();
+		}
+		else{
+			System.out.print("Erreur inconue");
+		}
 	}
 
 }
